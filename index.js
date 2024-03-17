d80f4f1f8e1d549ca70928f3eba63e55c53b3176
Hanan501 <hanan11.mohsin@gmail.com>
2024-03-18 01:35:00 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+function wrapGifts(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(`Wrapped ${array[0]} and added a bow!`);
+        
+    }
+}
+
+
+
+function writeCards(array, event){
+    let newarray = [];
+    for (let i = 0; i < array.length; i++) {
+        newarray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return newarray;
+}
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"))
+
+
+
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+
+// countDown(10);
